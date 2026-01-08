f0d736ad3abf21b411c9f570ca458b3ca864e9a5
T_H_0_M <thomasbandy23@gmail.com>
2026-01-08 23:37:50 +1100

    correct harpoon key

--- nvim/lua/thomas/plugins/harpoon.lua
+++ nvim/lua/thomas/plugins/harpoon.lua
@@ -15,7 +15,7 @@ return {
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end)
 
-		vim.keymap.set("n", "<C-y>", function()
+		vim.keymap.set("n", "<C-t>", function()
 			harpoon:list():select(1)
 		end)
 		vim.keymap.set("n", "<C-y>", function()
